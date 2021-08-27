7e9b649b75bd75c1c915849cdb653ff0d3298966
kinzhong <chewkinzhong@gmail.com>
2021-08-27 22:00:46 +0800

    add -y to install

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo apt update
-sudo apt upgrade
-sudo apt-get install \
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -11,8 +11,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
