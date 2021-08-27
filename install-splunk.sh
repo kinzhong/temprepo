023c5f057e538bbc944b2b80f2355d5b7e6cd710
kinzhong <chewkinzhong@gmail.com>
2021-08-27 21:53:40 +0800

    added install-splunk.sh

--- install-splunk.sh
+++ install-splunk.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+sudo docker pull splunk/splunk:latest
+sudo docker run -d -p 8000:8000 -p "8089:8089" -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=password" --name splunk splunk/splunk:latest
